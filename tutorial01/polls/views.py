e18531b83c844eef81aadd674f9e8fb8e4d10f13
Rainer Poisel <rainer.poisel@gmail.com>
2012-12-31 18:53:26 +0100

    additional views introduced

--- tutorial01/polls/views.py
+++ tutorial01/polls/views.py
@@ -1,5 +1,22 @@
 from django.http import HttpResponse
 
+from polls.models import Poll
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the poll index.")
+    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+    output = ', '.join([p.question for p in latest_poll_list])
+    return HttpResponse(output)
+
+
+def detail(request, poll_id):
+    return HttpResponse("You are looking at poll %s. " % poll_id)
+
+
+def results(request, poll_id):
+    return HttpResponse("You are looking at the results of poll %s. "
+            % poll_id)
+
+
+def vote(request, poll_id):
+    return HttpResponse("You are voting on poll %s. " % poll_id)
