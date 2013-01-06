a4474f060f8216554e5726d70c34d16b7eaeb918
Rainer Poisel <rainer.poisel@gmail.com>
2013-01-06 11:42:30 +0100

    tutorial 3 finished

--- tutorial01/polls/urls.py
+++ tutorial01/polls/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import patterns, url
 
 from polls import views
 
-urlpatterns = patterns('',
+urlpatterns = patterns('views',
     url(r'^$', views.index, name='index'),
-    # ex: /polls/5/
-    url(r'^(?P<poll_id>\d+)/$', views.detail, name='detail'),
-    url(r'^(?P<poll_id>\d+)/results/$', views.results, name='results'),
-    url(r'^(?P<poll_id>\d+)/vote/$', views.vote, name='vote'),
+    # example: /polls/5/
+    url(r'^(?P<poll_id>\d+)/$', detail, name='detail'),
+    url(r'^(?P<poll_id>\d+)/results/$', results, name='results'),
+    url(r'^(?P<poll_id>\d+)/vote/$', vote, name='vote'),
 )
--- tutorial01/polls/views.py
+++ tutorial01/polls/views.py
@@ -1,16 +1,41 @@
-from django.http import HttpResponse
+import os
+
+from django.template import Context, loader
+from django.http import HttpResponse, Http404
+from django.shortcuts import render_to_response, get_object_or_404
 
 from polls.models import Poll
 
 
 def index(request):
-    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
-    output = ', '.join([p.question for p in latest_poll_list])
-    return HttpResponse(output)
+    # latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+    latest_poll_list = Poll.objects.order_by('-pub_date')
+    # example: hard-coded
+    # output = ', '.join([p.question for p in latest_poll_list])
+    # example: explicitly rendered view
+    # t = loader.get_template(os.path.join('polls', 'index.html'))
+    # c = Context({
+    #    'latest_poll_list': latest_poll_list,
+    #    })
+    # return HttpResponse(t.render(c))
+    # example: implicitly rendered view
+    return render_to_response(
+            os.path.join('polls', 'index.html'),
+            {'latest_poll_list': latest_poll_list}
+            )
 
 
 def detail(request, poll_id):
-    return HttpResponse("You are looking at poll %s. " % poll_id)
+    # example: explicitly query for requested object
+    # try:
+    #    p = Poll.objects.get(pk=poll_id)
+    # except Poll.DoesNotExist:
+    #    raise Http404
+    p = get_object_or_404(Poll, pk=poll_id)
+    return render_to_response(os.path.join(
+        'polls', 'detail.html'),
+        {'poll': p}
+        )
 
 
 def results(request, poll_id):
@@ -20,3 +45,7 @@ def results(request, poll_id):
 
 def vote(request, poll_id):
     return HttpResponse("You are voting on poll %s. " % poll_id)
+
+
+def error(request):
+    return HttpResponse("This is the error-page that you requested. ")
