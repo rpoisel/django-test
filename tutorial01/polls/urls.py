43e7674c7516e1172f5992fdbf4ace0765316df4
Rainer Poisel <rainer.poisel@gmail.com>
2013-01-10 00:45:11 +0100

    usage of generic views

--- tutorial01/polls/urls.py
+++ tutorial01/polls/urls.py
@@ -1,11 +1,24 @@
-from django.conf.urls import patterns, url
+import os
 
-from polls import views
+from django.conf.urls import patterns, url
+from django.views.generic import DetailView, ListView
+
+from polls.models import Poll
 
 urlpatterns = patterns('polls.views',
-    url(r'^$', 'index'),
+    url(r'^$', ListView.as_view(
+            queryset=Poll.objects.order_by('-pub_date'),
+            context_object_name='latest_poll_list',
+            template_name=os.path.join('polls', 'index.html'))),
     # example: /polls/5/
-    url(r'^(?P<poll_id>\d+)/$', 'detail'),
-    url(r'^(?P<poll_id>\d+)/results/$', 'results'),
+    url(r'^(?P<pk>\d+)/$',
+        DetailView.as_view(
+            model=Poll,
+            template_name=os.path.join('polls', 'detail.html'))),
+    url(r'^(?P<pk>\d+)/results/$',
+        DetailView.as_view(
+            model=Poll,
+            template_name=os.path.join('polls', 'results.html')),
+        name='poll_results'),
     url(r'^(?P<poll_id>\d+)/vote/$', 'vote'),
 )
--- tutorial01/polls/views.py
+++ tutorial01/polls/views.py
@@ -5,47 +5,7 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.shortcuts import render_to_response, get_object_or_404
 
-from polls.models import Poll
-
-
-def index(request):
-    # latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
-    latest_poll_list = Poll.objects.order_by('-pub_date')
-    # example: hard-coded
-    # output = ', '.join([p.question for p in latest_poll_list])
-    # example: explicitly rendered view
-    # t = loader.get_template(os.path.join('polls', 'index.html'))
-    # c = Context({
-    #    'latest_poll_list': latest_poll_list,
-    #    })
-    # return HttpResponse(t.render(c))
-    # example: implicitly rendered view
-    return render_to_response(
-            os.path.join('polls', 'index.html'),
-            {'latest_poll_list': latest_poll_list}
-            )
-
-
-def detail(request, poll_id):
-    # example: explicitly query for requested object
-    # try:
-    #    p = Poll.objects.get(pk=poll_id)
-    # except Poll.DoesNotExist:
-    #    raise Http404
-    p = get_object_or_404(Poll, pk=poll_id)
-    return render_to_response(os.path.join(
-        'polls', 'detail.html'),
-        {'poll': p},
-        context_instance=RequestContext(request)
-        )
-
-
-def results(request, poll_id):
-    p = get_object_or_404(Poll, pk=poll_id)
-    return render_to_response(
-            os.path.join('polls', 'results.html'),
-            {'poll': p}
-            )
+from polls.models import Poll, Choice
 
 
 def vote(request, poll_id):
@@ -66,7 +26,7 @@ def vote(request, poll_id):
         # user hits the Back button.
         return HttpResponseRedirect(
                 reverse(
-                    'polls.views.results',
+                    'poll_results',
                     args=(p.id,)
                     )
                 )
